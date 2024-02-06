def2b776fab6bafc7cfd94d8dee374b8eb39ea05
Christoffer Strömberg <chris.stromberg@icloud.com>
2024-02-06 12:35:43 +0100

    updated example

--- notebooks/example_persontransportstatistik.py
+++ notebooks/example_persontransportstatistik.py
@@ -7,26 +7,19 @@
 # Import packages.
 
 # %%
-import json
-
 # %matplotlib inline
 import ifk_scb_compilations.objects.passenger_transport as passenger_transport
-import requests
 
 # %% [markdown]
 # Start a request session and fetch data from scb, based on the query defined in ifk_scb_compilations.queries.passenger_transport.Request_input.query.
 
 # %%
-session = requests.Session()
-response = session.post(
-    passenger_transport.Request_input.url, json=passenger_transport.Request_input.query
-)
-response_json = json.loads(response.content.decode("utf-8-sig"))
+scb_data = passenger_transport.FetchScbData()
 
 # %% [markdown]
 # Create an instance of passenger_transport.Analysis and execute the analysis, compiliation or whatever you want to do with the data.
 
 # %%
-passenger_analysis = passenger_transport.Analysis(response_json)
+passenger_analysis = passenger_transport.Analysis(scb_data.data)
 passenger_analysis.plot_co2_transports()
 passenger_analysis.plot_co2_national_international()
