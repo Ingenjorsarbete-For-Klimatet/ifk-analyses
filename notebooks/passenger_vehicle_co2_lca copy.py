45ea2c99963f25b6321f0bd463f1f68c6d1f85e1
Anders Nord <dr.anders.nord@gmail.com>
2024-10-30 06:42:18 +0100

    Initiated update of structure, modified package name

--- notebooks/passenger_vehicle_co2_lca copy.py
+++ notebooks/passenger_vehicle_co2_lca copy.py
@@ -29,7 +29,6 @@
 # körning. De exempel på siffror som tagits fram kommer från öppna källor på nätet, t ex Volvo Cars
 # LCA-rapporter, men lämnas för användaren att ändra för att kunna göra egna parameterstudier.
 
-import ifk_analyses.objects.personal_vehicle_lca as pvl
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -47,9 +46,8 @@ import numpy as np
 # Grundläggande antaganden
 car_manufacturing_cost = 8  # 8 kg CO2/kg car
 battery_manufacturing_cost = 77  # 77 kg CO2/kWh battery (average between 56 & 98, from EX90 and EX30, respectively.)
-kgCO2_per_kWh = 0.45  # 0.45 kg CO2/kWh electricity (World mix)
+kgCO2_per_kWh_europe = 0.45  # 0.45 kg CO2/kWh electricity (World mix)
 kgCO2_per_litre = 3  # 3kg C02/litre petrol (extraction, production & combustion)
-vehicle_life = 300000  # 300 000 km defined as the average life of a vehicle (from googling Swedish sources)
 
 # ## Definiera bilar
 # För att kunna göra jämförelsen behöver vi välja vilka biltyper vi vill titta på. Följande tre typexempel har tagits fram:
@@ -65,80 +63,53 @@ vehicle_life = 300000  # 300 000 km defined as the average life of a vehicle (fr
 #
 
 cars = {
-    pvl.Vehicle(
-        [
-            "A: Liten bensinbil",
-            1500,
-            0,
-            0.05,
-            1,
-            car_manufacturing_cost,
-            battery_manufacturing_cost,
-            kgCO2_per_litre,
-            vehicle_life,
-        ]
-    ),
-    pvl.Vehicle(
-        [
-            "B: Stor elbil",
-            2200,
-            125,
-            0.25,
-            1,
-            car_manufacturing_cost,
-            battery_manufacturing_cost,
-            kgCO2_per_kWh,
-            vehicle_life,
-        ]
-    ),
-    pvl.Vehicle(
-        [
-            "C: Liten elbil",
-            1500,
-            50,
-            0.15,
-            1,
-            car_manufacturing_cost,
-            battery_manufacturing_cost,
-            kgCO2_per_kWh,
-            vehicle_life,
-        ]
-    ),
-    pvl.Vehicle(
-        [
-            "D: Cykel",
-            10,
-            0,
-            0,
-            1,
-            car_manufacturing_cost,
-            battery_manufacturing_cost,
-            kgCO2_per_kWh,
-            vehicle_life,
-        ]
-    ),
-    pvl.Vehicle(
-        [
-            "E: Elcykel",
-            15,
-            0.5,
-            0.005,
-            1,
-            car_manufacturing_cost,
-            battery_manufacturing_cost,
-            kgCO2_per_kWh,
-            vehicle_life,
-        ]
-    ),
+    "A: Liten bensinbil": [1500, 0, 0, 0.05, 1],
+    "B: Stor elbil": [2200, 125, 500, 0.25, 1],
+    "C: Liten elbil": [1500, 50, 330, 0.15, 1],
+    "D: Cykel": [10, 0, 0, 0, 1],
+    "E: Elcykel": [15, 0.5, 20, 0.005, 1],
 }
+print(cars)
 
-driven_distance = np.linspace(0, vehicle_life, 200)
-legend = []
-for car in cars:
-    car_data = pvl.co2analysis(car, driven_distance, 1)
-    plt.plot(car_data[1], car_data[2])
-    legend.append(car.name[0])
-plt.legend(legend)
+
+# Definiera funktion som ger CO2 över tid
+def co2analysis(cars, driven_distance, co2debt):
+    """CO2 accumulator function."""
+    car_data = []
+    for key, value in cars.items():
+        C02_data_constant = (
+            co2debt
+            + car_manufacturing_cost * value[0]
+            + battery_manufacturing_cost * value[1]
+        )
+        if value[1] == 0:
+            C02_data = (
+                C02_data_constant
+                + kgC02_per_litre
+                * value[3]
+                * (driven_distance - driven_distance[0])
+                * value[4]
+            )
+        else:
+            C02_data = (
+                C02_data_constant
+                + kgC02_per_kWh_europe
+                * value[3]
+                * (driven_distance - driven_distance[0])
+                * value[4]
+            )
+        car_data.append([key, driven_distance, C02_data / 1000])
+
+    return car_data
+
+
+driven_distance = np.linspace(0, 300000, 200)
+leg = []
+car_data = co2analysis(cars, driven_distance, 0)
+for value in car_data:
+    plt.plot(value[1], value[2])
+    leg.append(value[0])
+plt.legend(leg)
 plt.xlabel("kördistans [km]")
 plt.ylabel("CO2 [metric tonnes]")
 plt.show()
@@ -289,36 +260,19 @@ plt.ylabel("CO2 [metric tonnes]")
 plt.show()
 
 # ## Från det personliga planet till världens fordonsflotta
-# Genom att tillgripa en naivistisk analys (vi ger inte någon rabatt då en bil
-# säljs, vilket innebär att inköp av begagnad bil skulle ge en initial-CO2-kostnad
-# av 0 ton och därmed räknar vi inte med avskrivning. Eftersom koldioxid ackumuleras
-# i atmosfären är det dock i vår mening det rimligaste sättet att förhålla sig till
-# problemet) har vi visat hur elektrifiering, i dagens SUV-tider, sällan innebär
-# sänkta koldioxidutsläpp, och en övergång till 10% cykel *i kombination med inköp
-# av elbil* endast är marginellt bättre.
+# Genom att tillgripa en naivistisk analys (vi ger inte någon rabatt då en bil säljs, vilket innebär att inköp av begagnad bil skulle ge en initial-CO2-kostnad av 0 ton och därmed räknar vi inte med avskrivning. Eftersom koldioxid ackumuleras i atmosfären är det dock i vår mening det rimligaste sättet att förhålla sig till problemet) har vi visat hur elektrifiering, i dagens SUV-tider, sällan innebär sänkta koldioxidutsläpp, och en övergång till 10% cykel *i kombination med inköp av elbil* endast är marginellt bättre.
 #
-# Vad har vi för siffror att förhålla oss till på samhällsnivå? I världen
-# finns det grovt räknat två miljarder bilar. Hur gamla dessa är har jag
-# svårt att hitta siffror för, men den omfattande svenska statistiken har
-# siffror som vi kan tillgripa: I Sverige är en bil i genomsnitt 10år gammal,
-# och skrotas i genomsnitt efter 18år.
+# Vad har vi för siffror att förhålla oss till på samhällsnivå? I världen finns det grovt räknat två miljarder bilar. Hur gamla dessa är har jag svårt att hitta siffror för, men den omfattande svenska statistiken har siffror som vi kan tillgripa: I Sverige är en bil i genomsnitt 10år gammal, och skrotas i genomsnitt efter 18år.
 #
-# Låt oss vidare anta att de svenska förhållandena är representativa för
-# världen i stort och att en bil körs 300000km i genomsnitt innan den skrotas
-# (vilket är siffror som dyker upp vid googling - jag hittar inte någon siffra
-# från scb eller trafa för att bekräfta detta för tillfället).
+# Låt oss vidare anta att de svenska förhållandena är representativa för världen i stort och att en bil körs 300000km i genomsnitt innan den skrotas (vilket är siffror som dyker upp vid googling - jag hittar inte någon siffra från scb eller trafa för att bekräfta detta för tillfället).
 #
-# Slutligen antar vi att bilarna rullar med konstant körsträcka varje år. Det
-# innebär alltså att en tio år gammal genomsnittsbil har:
+# Slutligen antar vi att bilarna rullar med konstant körsträcka varje år. Det innebär alltså att en tio år gammal genomsnittsbil har:
 #
 # $$
 # \frac{(18-10)}{18}*300000=133333\text{km}
 # $$
 #
-# kvar att köra innan den skrotas. Anta nu att de allra flesta bilar i
-# världsflottan fortfarande är bensinbilar, och att de i genomsnitt har en
-# förbrukning om 0,7l/mil. Vi kan då räkna ut hur många miljarder ton CO2
-# den befintliga fordonsflottan förväntas släppa ut innan den är förbrukad:
+# kvar att köra innan den skrotas. Anta nu att de allra flesta bilar i världsflottan fortfarande är bensinbilar, och att de i genomsnitt har en förbrukning om 0,7l/mil. Vi kan då räkna ut hur många miljarder ton CO2 den befintliga fordonsflottan förväntas släppa ut innan den är förbrukad:
 
 # +
 average_remaining_distance = 133333  # km
@@ -340,25 +294,17 @@ print("{:3.0f} Gton CO2".format(remaining_CO2_output_from_current_fleet))
 # -
 
 # ### Hur mycket är det?
-# För att sätta denna siffra i kontext är det lämpligt att gå till IPCC-referensen
-# ovan. När det kommer till kvarvarande budget för koldioxidekvivalentutsläpp säger
-# rapporten detta:
+# För att sätta denna siffra i kontext är det lämpligt att gå till IPCC-referensen ovan. När det kommer till kvarvarande budget för koldioxidekvivalentutsläpp säger rapporten detta:
 #
-# > *For every 1000 GtCO2 emitted by human activity, global surface temperature
-# rises by 0.45°C (best estimate, with a likely range from 0.27°C to 0.63°C).
-# The best estimates of the remaining carbon budgets from the beginning of 2020
-# are 500 GtCO2 for a 50% likelihood of limiting global warming to 1.5°C and 1150
-# GtCO2 for a 67% likelihood of limiting warming to 2°C. The stronger the reductions
-# in non-CO2 emissions, the lower the resulting temperatures are for a given
-# remaining carbon budget or the larger remaining carbon budget for the same
-# level of temperature change.*
+# > *For every 1000 GtCO2 emitted by human activity, global surface temperature rises by 0.45°C (best estimate, with a likely
+# range from 0.27°C to 0.63°C). The best estimates of the remaining carbon budgets from the beginning of 2020 are
+# 500 GtCO2 for a 50% likelihood of limiting global warming to 1.5°C and 1150 GtCO2 for a 67% likelihood of limiting
+# warming to 2°C. The stronger the reductions in non-CO2 emissions, the lower the resulting temperatures are for a given
+# remaining carbon budget or the larger remaining carbon budget for the same level of temperature change.*
 #
-# Av de 500 GtCO2 som fanns att tillgå 2020 har omkring 4/5 redan förbrukats.
-# Av den kvarvarande budgeten äter alltså redan den befintliga fordonsflottan
-# upp omkring hälften.
+# Av de 500 GtCO2 som fanns att tillgå 2020 har omkring 4/5 redan förbrukats. Av den kvarvarande budgeten äter alltså redan den befintliga fordonsflottan upp omkring hälften.
 #
-# Om vi väljer att växla till en större elbil för alla bilar så kommer den att
-# öka mängden C02-utsläpp till totalt resta 3000000km med:
+# Om vi väljer att växla till en större elbil för alla bilar så kommer den att öka mängden C02-utsläpp till totalt resta 3000000km med:
 
 increase_from_current_fleet_if_electrified = car_data[1][2][-1] / car_data[0][2][-1]
 print("{:3.0f} percent".format((increase_from_current_fleet_if_electrified - 1) * 100))
@@ -374,9 +320,6 @@ print("when everyone changes to an electric car now, would be approximately")
 print("{:3.0f} Gton CO2".format(remaining_CO2_output_from_current_fleet_if_electrified))
 
 
-# De nya elbilarna, i egenskap av nya, skulle därtill fortsätta släppa ut CO2
-# så länge inte all elektricitet kommer från fossilfria alternativ i ytterligare
-# nästan 170000km.
+# De nya elbilarna, i egenskap av nya, skulle därtill fortsätta släppa ut CO2 så länge inte all elektricitet kommer från fossilfria alternativ i ytterligare nästan 170000km.
 
-# Denna analys har inte tagit hänsyn till CO2-kostnaden för att skapa och
-# upprätthålla väginfrastruktur för dessa miljardvis med vägbundna fordon.
+# Denna analys har inte tagit hänsyn till CO2-kostnaden för att skapa och upprätthålla väginfrastruktur för dessa miljardvis med vägbundna fordon.
--- src/ifk_analyses/objects/personal_vehicle_lca.py
+++ src/ifk_analyses/objects/personal_vehicle_lca.py
@@ -1,7 +1,5 @@
 """Backend data class for vehicle."""
 
-import numpy as np
-
 
 class Vehicle:
     """Class for vehicles."""
@@ -12,11 +10,10 @@ class Vehicle:
         self.weight = data[1]
         self.battery_capacity = data[2]
         self.consumption_per_km = data[3]
-        self.usage = data[4]
-        self.co2_build_cost_per_kg = data[5]
-        self.co2_battery_build_cost_per_kWh = data[6]
-        self.co2_cost_per_consumption = data[7]
-        self.vehicle_life_km = data[8]
+        self.co2_build_cost_per_kg = data[4]
+        self.co2_battery_build_cost_per_kWh = data[5]
+        self.co2_cost_per_consumption = data[6]
+        self.vehicle_life_km = data[7]
 
 
 def co2analysis(car: Vehicle, driven_distance, co2debt):
@@ -25,14 +22,13 @@ def co2analysis(car: Vehicle, driven_distance, co2debt):
         co2debt
         + car.co2_build_cost_per_kg * car.weight
         + car.co2_battery_build_cost_per_kWh * car.battery_capacity
-        )
+    )
     C02_data = (
         C02_data_constant
         + car.co2_cost_per_consumption
         * car.consumption_per_km
         * (driven_distance - driven_distance[0])
-        * car.usage
-        )
+    )
     car_data = [car, driven_distance, C02_data]
 
     return car_data
